31d6eebbb8a88d5d5b0fd4cc1d5395109328c903
nahyun0423 <skgus0423@naver.com>
2023-09-11 16:23:26 +0900

    chore: class 분리 수정

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
     StringCalculator stringCalculator;
-
+    Calculator calculator;
 
     @BeforeAll
     public void setUp() {
         stringCalculator = new StringCalculator();
+        calculator = new Calculator();
     }
 
 
     @Test
     public void split() {
         String actual = "1 + 2 * 3";
-        assertThat(stringCalculator.StringToArray(actual))
+        assertThat(calculator.StringToArray(actual))
                 .contains("1")
                 .contains("+")
                 .contains("2")
@@ -29,7 +29,7 @@ public class CalculatorTest {
 
     @Test
     public void makeResultTest() {
-        int actual = stringCalculator.makeResult("1 + 2 * 3");
+        int actual = calculator.makeResult("1 + 2 * 3");
         assertThat(actual).isEqualTo(9);
     }
 }
--- src/test/java/study/StringCalculator.java
+++ src/test/java/study/StringCalculator.java
@@ -5,71 +5,12 @@ import java.util.Scanner;
 
 public class StringCalculator {
 
-    Scanner scanner = new Scanner(System.in);
-    String value = scanner.nextLine();
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
-    public String[] StringToArray(String value) {
-        return value.split(" ");
-    }
-// 입력 받고, 공백으로 분리
-
-    public int toInt(String value) {
-        return Integer.parseInt(value);
-    }
-
-    public int calculate(int first, char operator, int second) {
-        if (operator == '+') {
-            return add(first, second);
-        }
-        if (operator == '-') {
-            return sub(first, second);
-        }
-        if (operator == '/') {
-            return divide(first, second);
-        }
-        if (operator == '*') {
-            return multiply(first, second);
-        }
-        else System.out.println("잘못된 값 입력");
-
-        return first;
-    }
-
-    int add(int first, int second) {
-        return first + second;
-    }
-
-    int sub(int first, int second) {
-        return first - second;
-    }
-
-    int multiply(int first, int second) {
-        return first * second;
-    }
-
-    int divide(int first, int second) {
-        try {
-            return first / second;
-        } catch (Exception e) {
-            System.out.println("0으로 나눌 수 없습니다.");
-        }
-        return 0;
-    }
-//계산 메소드
-
-
-    public int getResult(String[] value) {
-        int result = toInt(value[0]);
-
-        for (int i = 0; i < (value.length) - 2; i += 2) {
-            result = calculate(result, value[i + 1].charAt(0), toInt(value[i + 2]));
-        }
-        return result;
-    }
-
-    //순서대로 두개씩 계산
-    public int makeResult(String value) {
-        return getResult(StringToArray(value));
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        calculator.makeResult(value);
     }
 
 }
